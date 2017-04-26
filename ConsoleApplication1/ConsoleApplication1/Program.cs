a413566001783ec01c1d41bee1cb462fc4542237
Tony <tony.lilui@live.com>
2017-04-26 19:27:42 -0400

    Today I learned how to make a string variable and how to display its content to the console.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,16 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Out.WriteLine("Hello World!");
+            string sentence; // I created a variable called sentence which is the type "string"
+            sentence = "Hello Tony! \nhello henry "; // I assigned a value to my "sentence" variable
+            Console.Out.WriteLine(sentence);// This displays the "sentence" variable in the console
+            sentence = "2 plus 1 equals 3";
+            Console.Out.WriteLine(sentence);
+            sentence = "hahaha";
+            Console.Out.WriteLine(sentence);
+
+
+
         }
     }
 }
