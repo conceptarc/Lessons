b7394af2549ab415861534d67c86dc890061b846
Tony <tony.lilui@live.com>
2017-04-27 17:57:09 -0400

    The program reads what is on the console and writes it back to the console. The program gives my variable a value depending on what is read from the console.

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // the equal sign assigns the right side to the left side
+            String words = Console.In.ReadLine(); /* I gave words the value of Console.In.ReadLine
+                                                   * so the program would give words a value from the
+                                                   * console
+            Console.In.ReadLine();// it take what is in the console and reads it to the program
+                                  //* I first have to put something into the console. Then the program will take what is in the console and read it to the program. 
+                                  /*  Then the program will take what is in the console and save it into the string. 
+                                   *  Then it will assign a value to my variable
+                                   */
+            Console.Out.WriteLine(words);
+
+            //* after the program reads the input it gives words a value of what i input
+              /* and then the program writes the value of words to the console
+              */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
